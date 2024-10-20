64444e3db5a3a379ab3cbf5b587fe3c46be28700
lewisblackburn <lewisblackburn10@gmail.com>
2024-10-20 14:42:49 +0100

    fix(seed): update roles and permissions in migration file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,6 +23,47 @@ async function seed() {
 
 	await cleanUpDatabase()
 
+	console.time('🔑 Created permissions...')
+	const entities = ['user', 'note', 'film', 'filmReview']
+	const actions = ['create', 'read', 'update', 'delete']
+	const accesses = ['own', 'any'] as const
+
+	let permissionsToCreate = []
+	for (const entity of entities) {
+		for (const action of actions) {
+			for (const access of accesses) {
+				permissionsToCreate.push({ entity, action, access })
+			}
+		}
+	}
+	await prisma.permission.createMany({ data: permissionsToCreate })
+	console.timeEnd('🔑 Created permissions...')
+
+	console.time('👑 Created roles...')
+	await prisma.role.create({
+		data: {
+			name: 'admin',
+			permissions: {
+				connect: await prisma.permission.findMany({
+					select: { id: true },
+					where: { access: 'any' },
+				}),
+			},
+		},
+	})
+	await prisma.role.create({
+		data: {
+			name: 'user',
+			permissions: {
+				connect: await prisma.permission.findMany({
+					select: { id: true },
+					where: { access: 'own' },
+				}),
+			},
+		},
+	})
+	console.timeEnd('👑 Created roles...')
+
 	await createUsers()
 
 	await createAdminUser()
