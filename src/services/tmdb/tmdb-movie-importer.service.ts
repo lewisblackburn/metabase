448fcc3edf04e306f24ace941155b3d39edaa5d5
lewisblackburn <lewisblackburn10@gmail.com>
2025-04-20 13:43:46 +0100

    feat(tmdb movie importer): added return types to build functions

--- src/services/tmdb/tmdb-movie-importer.service.ts
+++ src/services/tmdb/tmdb-movie-importer.service.ts
@@ -2,20 +2,29 @@ import { LANGUAGES } from '@/constants/languages.constant';
 import { MEDIA_TYPE } from '@/constants/media.constant';
 import {
     Certifications_Constraint,
+    Certifications_Obj_Rel_Insert_Input,
     Certifications_Update_Column,
     Genres_Constraint,
     Genres_Update_Column,
     GetMovieByTmdb_IdDocument,
     GetMovieByTmdb_IdQuery,
     GetMovieByTmdb_IdQueryVariables,
+    InputMaybe,
     InsertMovieDocument,
     InsertMovieMutation,
     InsertMovieMutationVariables,
     Keywords_Constraint,
     Keywords_Update_Column,
+    Movie_Alternative_Titles_Arr_Rel_Insert_Input,
+    Movie_Cast_Arr_Rel_Insert_Input,
+    Movie_Crew_Arr_Rel_Insert_Input,
+    Movie_Genres_Arr_Rel_Insert_Input,
+    Movie_Keywords_Arr_Rel_Insert_Input,
     Movie_Media_Constraint,
+    Movie_Production_Companies_Arr_Rel_Insert_Input,
     Movie_Production_Companies_Constraint,
     Movie_Production_Companies_Update_Column,
+    Movie_Production_Countries_Arr_Rel_Insert_Input,
     Movies_Insert_Input,
     People_Constraint,
     People_Update_Column,
@@ -28,7 +37,7 @@ import { FileService } from '../file.service';
 import { TMDBService } from './tmdb.service';
 
 export class TMDBMovieImporterService extends TMDBService {
-    private buildKeywords(movie: TMDBMovie) {
+    private buildKeywords(movie: TMDBMovie): InputMaybe<Movie_Keywords_Arr_Rel_Insert_Input> | undefined {
         return {
             data: movie.keywords.keywords.map((k) => ({
                 keyword: {
@@ -42,7 +51,7 @@ export class TMDBMovieImporterService extends TMDBService {
         };
     }
 
-    private buildGenres(movie: TMDBMovie) {
+    private buildGenres(movie: TMDBMovie): InputMaybe<Movie_Genres_Arr_Rel_Insert_Input> | undefined {
         return {
             data: movie.genres.map((g, idx) => ({
                 genre: {
@@ -57,102 +66,117 @@ export class TMDBMovieImporterService extends TMDBService {
         };
     }
 
-    private async buildCast(movie: TMDBMovie) {
+    private async buildCast(movie: TMDBMovie): Promise<InputMaybe<Movie_Cast_Arr_Rel_Insert_Input> | undefined> {
         const uploads = await Promise.all(
             movie.credits.cast.map((c) =>
                 FileService.uploadImage(c.profile_path, MEDIA_TYPE.HEADSHOT, this.getProfileImage)
             )
         );
-        return movie.credits.cast.map((c, idx) => {
-            const file = uploads[idx];
-            return {
-                character: c.character,
-                order: idx + 1,
-                person: {
-                    data: {
-                        tmdb_id: c.id.toString(),
-                        name: c.name,
-                        headshot: file?.url,
-                        person_media: file
-                            ? {
-                                  data: [
-                                      {
-                                          media_type: MEDIA_TYPE.HEADSHOT,
-                                          media_url: file.url,
-                                          media_id: file.id
+        return {
+            data: movie.credits.cast.map((c, idx) => {
+                const file = uploads[idx];
+                return {
+                    character: c.character,
+                    order: idx + 1,
+                    person: {
+                        data: {
+                            tmdb_id: c.id.toString(),
+                            name: c.name,
+                            headshot: file?.url,
+                            person_media: file
+                                ? {
+                                      data: [
+                                          {
+                                              media_type: MEDIA_TYPE.HEADSHOT,
+                                              media_url: file.url,
+                                              media_id: file.id
+                                          }
+                                      ],
+                                      on_conflict: {
+                                          constraint: Person_Media_Constraint.PersonMediaPkey
                                       }
-                                  ],
-                                  on_conflict: {
-                                      constraint: Person_Media_Constraint.PersonMediaPkey
                                   }
-                              }
-                            : undefined
-                    },
-                    on_conflict: {
-                        constraint: People_Constraint.PeopleTmdbIdKey,
-                        update_columns: [People_Update_Column.Headshot]
+                                : undefined
+                        },
+                        on_conflict: {
+                            constraint: People_Constraint.PeopleTmdbIdKey,
+                            update_columns: [People_Update_Column.Headshot]
+                        }
                     }
-                }
-            };
-        });
+                };
+            })
+        };
     }
 
-    private async buildCrew(movie: TMDBMovie) {
+    private async buildCrew(movie: TMDBMovie): Promise<InputMaybe<Movie_Crew_Arr_Rel_Insert_Input> | undefined> {
         const uploads = await Promise.all(
             movie.credits.crew.map((c) =>
                 FileService.uploadImage(c.profile_path, MEDIA_TYPE.HEADSHOT, this.getProfileImage)
             )
         );
-        return movie.credits.crew.map((c, idx) => {
-            const file = uploads[idx];
-            return {
-                job: c.job,
-                department: c.department,
-                person: {
-                    data: {
-                        tmdb_id: c.id.toString(),
-                        name: c.name,
-                        headshot: file?.url,
-                        person_media: file
-                            ? {
-                                  data: [
-                                      {
-                                          media_type: MEDIA_TYPE.HEADSHOT,
-                                          media_url: file.url,
-                                          media_id: file.id
+        return {
+            data: movie.credits.crew.map((c, idx) => {
+                const file = uploads[idx];
+                return {
+                    job: c.job,
+                    department: c.department,
+                    person: {
+                        data: {
+                            tmdb_id: c.id.toString(),
+                            name: c.name,
+                            headshot: file?.url,
+                            person_media: file
+                                ? {
+                                      data: [
+                                          {
+                                              media_type: MEDIA_TYPE.HEADSHOT,
+                                              media_url: file.url,
+                                              media_id: file.id
+                                          }
+                                      ],
+                                      on_conflict: {
+                                          constraint: Person_Media_Constraint.PersonMediaPkey
                                       }
-                                  ],
-                                  on_conflict: {
-                                      constraint: Person_Media_Constraint.PersonMediaPkey
                                   }
-                              }
-                            : undefined
-                    },
-                    on_conflict: {
-                        constraint: People_Constraint.PeopleTmdbIdKey,
-                        update_columns: [People_Update_Column.Headshot]
+                                : undefined
+                        },
+                        on_conflict: {
+                            constraint: People_Constraint.PeopleTmdbIdKey,
+                            update_columns: [People_Update_Column.Headshot]
+                        }
                     }
-                }
-            };
-        });
+                };
+            })
+        };
     }
 
-    private buildCountries(movie: TMDBMovie) {
+    private buildCountries(movie: TMDBMovie): InputMaybe<Movie_Production_Countries_Arr_Rel_Insert_Input> | undefined {
         return {
             data: movie.production_countries.map((c) => ({ country: c.name }))
         };
     }
 
-    private buildCompanies(movie: TMDBMovie) {
+    private async buildCompanies(
+        movie: TMDBMovie
+    ): Promise<InputMaybe<Movie_Production_Companies_Arr_Rel_Insert_Input> | undefined> {
         // const productionCompanyLogos = await Promise.all(
         //     movie.production_companies.map((company) =>
         //         FileService.uploadFile(this.getLogoImage(company.logo_path), MEDIA_TYPE.LOGO)
         //     )
         // );
-        return movie.production_companies.map((c) => ({ company_name: c.name }));
+
+        return {
+            data: movie.production_companies.map((c) => ({ company_name: c.name })),
+            on_conflict: {
+                constraint: Movie_Production_Companies_Constraint.MovieProductionCompaniesCompanyNameKey,
+                update_columns: [Movie_Production_Companies_Update_Column.CompanyName]
+            }
+        };
     }
 
-    private buildAlternativeTitles(movie: TMDBMovie) {
+    private buildAlternativeTitles(
+        movie: TMDBMovie
+    ): InputMaybe<Movie_Alternative_Titles_Arr_Rel_Insert_Input> | undefined {
         return {
             data: movie.alternative_titles.titles
                 .filter((t) => LANGUAGES.some((l) => l.code === t.iso_3166_1.toLowerCase()))
@@ -165,7 +189,7 @@ export class TMDBMovieImporterService extends TMDBService {
     }
 
     // NOTE: This only stores GB/US certification for now
-    private buildCertification(movie: TMDBMovie) {
+    private buildCertification(movie: TMDBMovie): InputMaybe<Certifications_Obj_Rel_Insert_Input> | undefined {
         const certificationName =
             (
                 movie.release_dates.results.find((r) => r.iso_3166_1 === 'GB') ||
@@ -217,10 +241,6 @@ export class TMDBMovieImporterService extends TMDBService {
             FileService.uploadImage(movie.backdrop_path, MEDIA_TYPE.BACKDROP, this.getBackdropImage)
         ]);
 
-        const castData = await this.buildCast(movie);
-        const crewData = await this.buildCrew(movie);
-        const productionCompaniesData = this.buildCompanies(movie);
-
         const payload = await this.save({
             title: movie.title,
             tagline: movie.tagline,
@@ -244,16 +264,10 @@ export class TMDBMovieImporterService extends TMDBService {
             },
             movie_keywords: this.buildKeywords(movie),
             movie_genres: this.buildGenres(movie),
-            movie_cast_members: { data: castData },
-            movie_crew_members: { data: crewData },
+            movie_cast_members: await this.buildCast(movie),
+            movie_crew_members: await this.buildCrew(movie),
             movie_production_countries: this.buildCountries(movie),
-            movie_production_companies: {
-                data: productionCompaniesData,
-                on_conflict: {
-                    constraint: Movie_Production_Companies_Constraint.MovieProductionCompaniesCompanyNameKey,
-                    update_columns: [Movie_Production_Companies_Update_Column.CompanyName]
-                }
-            },
+            movie_production_companies: await this.buildCompanies(movie),
             movie_alternative_titles: this.buildAlternativeTitles(movie),
             certification: this.buildCertification(movie)
         });
