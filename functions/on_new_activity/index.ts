efe12c048ee7f9ab7f547ba7777bd2c23dfb4145
lewisblackburn <lewisblackburn10@gmail.com>
2025-05-04 20:15:07 +0100

    feat(function): testing functions

--- functions/on_new_activity/index.ts
+++ functions/on_new_activity/index.ts
@@ -1,31 +1,40 @@
 // https://lwmecktyyhputyqkdigy.functions.eu-west-2.nhost.run/v1/on_new_activity
 // NOTE: This function is triggered when a new activity is inserted into the database by a Hasura event trigger.
-import {
-    GetFollowersDocument,
-    GetFollowersQuery,
-    GetFollowersQueryVariables,
-    UpsertNotificationsDocument,
-    UpsertNotificationsMutation,
-    UpsertNotificationsMutationVariables
-} from '@/generated/graphql';
 import { nhost } from '@/lib/nhost';
 
 import { Request, Response } from 'express';
+import { gql } from 'graphql-request';
+
+const followersQuery = gql`
+    query GetFollowers($user_id: uuid!) {
+        user(id: $user_id) {
+            followers {
+                follower {
+                    user_activities {
+                        id
+                    }
+                }
+            }
+        }
+    }
+`;
 
 export default async (req: Request, res: Response) => {
     const activity = req.body.event.data.new;
 
-    const followers = await nhost.graphql.request<GetFollowersQuery, GetFollowersQueryVariables>(GetFollowersDocument, {
+    const followers = await nhost.graphql.request(followersQuery, {
         user_id: activity.user_id
     });
 
-    const notifications = followers.data?.user?.followers.map((follower) => ({
-        recipient_id: follower.follower.id,
-        actor_id: activity.user_id,
-        activity_id: activity.id
-    }));
+    res.status(200).send(JSON.stringify(followers));
 
-    res.status(200).send(JSON.stringify(notifications));
+    // const notifications = followers.data?.user?.followers.map((follower: any) => ({
+    //     recipient_id: follower.follower.id,
+    //     actor_id: activity.user_id,
+    //     activity_id: activity.id
+    // }));
+
+    // res.status(200).send(JSON.stringify(notifications));
 
     // if (!notifications) {
     //     res.status(200).send('No notifications to insert');
