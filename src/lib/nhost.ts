19747025e6240f44457b12a14da33b06de34ac5e
lewisblackburn <lewisblackburn10@gmail.com>
2025-05-04 20:45:48 +0100

    feat(env): change env var to not be public

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -3,5 +3,5 @@ import { NhostClient } from '@nhost/nextjs';
 export const nhost = new NhostClient({
     subdomain: 'lwmecktyyhputyqkdigy',
     region: 'eu-west-2',
-    adminSecret: process.env.NEXT_PUBLIC_NHOST_ADMIN_SECRET || ''
+    adminSecret: process.env.NHOST_ADMIN_SECRET || ''
 });
