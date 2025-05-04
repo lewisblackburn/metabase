9de0b6c80a2167d840ea07f4be0091d92c38c23c
lewisblackburn <lewisblackburn10@gmail.com>
2025-05-04 20:48:06 +0100

    feat(env): more env var changes

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -1,7 +1,7 @@
 import { NhostClient } from '@nhost/nextjs';
 
 export const nhost = new NhostClient({
-    subdomain: 'lwmecktyyhputyqkdigy',
-    region: 'eu-west-2',
+    subdomain: process.env.NHOST_SUBDOMAIN || '',
+    region: process.env.NHOST_REGION || '',
     adminSecret: process.env.NHOST_ADMIN_SECRET || ''
 });
