cc7767ca3dfcd3cfdf3f5f2fb0d4f8c8debb11c6
lewisblackburn <lewisblackburn10@gmail.com>
2025-05-04 20:33:46 +0100

    feat(functions): this should work but i have to log out notifications again

--- functions/on_new_activity/index.ts
+++ functions/on_new_activity/index.ts
@@ -1,5 +1,6 @@
 // https://lwmecktyyhputyqkdigy.functions.eu-west-2.nhost.run/v1/on_new_activity
 // NOTE: This function is triggered when a new activity is inserted into the database by a Hasura event trigger.
+// NOTE: It is used to create notifications for the followers of the user who created the activity.
 import {
     UpsertNotificationsDocument,
     UpsertNotificationsMutation,
@@ -34,6 +35,8 @@ export default async (req: Request, res: Response) => {
         actor_id: activity.user_id,
         activity_id: activity.id
     }));
+
+    res.status(200).send(JSON.stringify(notifications));
 
     if (!notifications) {
         res.status(200).send('No notifications to insert');
