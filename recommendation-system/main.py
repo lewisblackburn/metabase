1b8e7f4f6275ce9c14573ba62fba275111b0d2d1
lewisblackburn <lewisblackburn10@gmail.com>
2025-05-23 17:18:27 +0100

    fix(recommendation-system): changed launch statement

--- recommendation-system/main.py
+++ recommendation-system/main.py
@@ -143,4 +143,9 @@ def recommend(user_id: str):
     return recs
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
