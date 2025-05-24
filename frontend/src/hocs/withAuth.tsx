fbafcc1f40d47015364dfbb590a0d6f0626a848d
lewisblackburn <lewisblackburn10@gmail.com>
2025-05-24 12:49:14 +0100

    feat(withAuth): trying to use a different hook and renamed component to a more accurate name

--- frontend/src/hocs/withAuth.tsx
+++ frontend/src/hocs/withAuth.tsx
@@ -1,41 +1,27 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { ComponentType, useEffect } from 'react';
 
 import { usePathname, useRouter } from 'next/navigation';
 
 import DefaultLoading from '@/components/shared/default-loading';
-import { nhost } from '@/lib/nhost';
+import { useAuthenticationStatus } from '@nhost/nextjs';
 
-export const withAuth = <P extends object>(Component: React.ComponentType<P>) => {
+export function withAuth<P extends object>(Wrapped: ComponentType<P>) {
     return function WithAuth(props: P) {
         const router = useRouter();
         const pathname = usePathname();
-        const [isLoading, setIsLoading] = useState(true);
+
+        const { isAuthenticated, isLoading } = useAuthenticationStatus();
 
         useEffect(() => {
-            const unsubscribe = nhost.auth.onAuthStateChanged((event) => {
-                if (event === 'SIGNED_IN') {
-                    setIsLoading(false);
-                } else if (event === 'SIGNED_OUT') {
-                    router.push(`/authentication/login?redirect=${encodeURIComponent(pathname)}`);
-                }
-            });
-
-            const isAuthenticated = nhost.auth.isAuthenticated();
-            if (!isAuthenticated) {
-                router.push(`/authentication/login?redirect=${encodeURIComponent(pathname)}`);
-            } else {
-                setIsLoading(false);
+            if (!isLoading && !isAuthenticated) {
+                router.replace(`/authentication/login?redirect=${encodeURIComponent(pathname)}`);
             }
-
-            return () => {
-                unsubscribe();
-            };
-        }, [router, pathname]);
+        }, [isLoading, isAuthenticated, pathname, router]);
 
         if (isLoading) return <DefaultLoading />;
 
-        return <Component {...props} />;
+        return <Wrapped {...props} />;
     };
-};
+}
