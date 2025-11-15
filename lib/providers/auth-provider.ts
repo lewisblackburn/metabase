a324aa1a5916b8eaea3597552dfb0a9c2c9546a3
Lewis Blackburn <lewis.blackburn@ballys.com>
2025-11-15 20:38:11 +0000

    fix(user): fix user auth

--- lib/providers/auth-provider.ts
+++ lib/providers/auth-provider.ts
@@ -1,15 +1,22 @@
 'use client'
 
 import { User } from '@nhost/nhost-js/auth'
-import { useMemo } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { useAuthStore } from '../stores/auth.store'
 
 export function AuthHydration({ user }: { user: User | undefined }) {
     const setUser = useAuthStore(s => s.setUser)
 
-    // use memo to prevent re-rendering when the user is the same
-    useMemo(() => setUser(user), [user, setUser])
+    // use ref to store the previous user id from previous render
+    const prevUserIdRef = useRef<string | undefined>(user?.id)
 
+    // only update the user when the user id changes to avoid unnecessary re-renders
+    useEffect(() => {
+        if (prevUserIdRef.current !== user?.id) {
+            setUser(user)
+        }
+        prevUserIdRef.current = user?.id
+    }, [user, setUser])
     return null
 }
