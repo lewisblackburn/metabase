8a13477ec6e4221f2560d24d0b87fbf3d2d90e87
Lewis Blackburn <lewis.blackburn@ballys.com>
2025-11-19 15:06:09 +0000

    fix create movie helper

--- lib/actions/movies/create.ts
+++ lib/actions/movies/create.ts
@@ -5,27 +5,16 @@ import {
     CreateMovieMutation,
     CreateMovieMutationVariables,
 } from '@/generated/graphql'
-import { Action, EntityType } from '@/lib/helpers/graphql-enums'
 import { createNhostClient } from '@/lib/nhost/server'
 import { handleGraphQLError } from '@/lib/utils/error-handler'
-
-import { withAudit } from '../audit-logs/with-audit'
 
 export async function createMovie({ title }: { title: string }) {
     const nhost = await createNhostClient()
 
-    return withAudit({
-        entityType: EntityType.MOVIE,
-        action: Action.INSERT,
-        operation: async () => {
-            return nhost.graphql
-                .request<CreateMovieMutation, CreateMovieMutationVariables>(CreateMovieDocument, {
-                    object: { title },
-                    on_conflict: { constraint: 'movies_pkey' },
-                })
-                .catch(handleGraphQLError)
-        },
-        getEntityId: result => result?.body.data?.insert_movies_one?.id,
-        getMetadata: () => ({ title }),
-    })
+    return nhost.graphql
+        .request<CreateMovieMutation, CreateMovieMutationVariables>(CreateMovieDocument, {
+            object: { title },
+            on_conflict: { constraint: 'movies_pkey' },
+        })
+        .catch(handleGraphQLError)
 }
