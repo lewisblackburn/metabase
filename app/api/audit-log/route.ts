5802378f0881fd26632f7ca3eb5aaa6cdca9c018
Lewis Blackburn <lewis.blackburn@ballys.com>
2025-11-19 14:44:34 +0000

    fix(audits): log data

--- app/api/audit-log/route.ts
+++ app/api/audit-log/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
     const { body } = await request.json()
+    const { id, table, event, data, op, session_variables } = body
+    const tableName = table.name
+    const { old, new: newData } = data
+    const userId = session_variables['x-hasura-user-id']
+
+    console.log({ body, id, tableName, event, old, newData, op, userId })
 
     // const auditLog = await createAuditLog({ entityType, action, entityId, metadata })
 
