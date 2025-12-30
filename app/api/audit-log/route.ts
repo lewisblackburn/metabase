72e77f7edd7f37e13eb9d87ca4fcab78630d027c
Lewis Blackburn <lewis.blackburn@ballys.com>
2025-12-30 14:58:19 +0000

    audit log

--- app/api/audit-log/route.ts
+++ app/api/audit-log/route.ts
@@ -1,30 +1,31 @@
+import gql from 'graphql-tag'
 import { NextResponse } from 'next/server'
 
-import { insertAuditLog } from '@/lib/actions/audit-logs/insert-audit-log'
+import {
+    Activity_Logs_Constraint,
+    InsertActivityLogDocument,
+    InsertActivityLogMutation,
+    InsertActivityLogMutationVariables,
+} from '@/generated/graphql'
 import { computeDataDifference, createAuditLogEntry } from '@/lib/helpers/audit-log-helpers'
+import { createAdminNhostClient } from '@/lib/nhost/admin-server'
 import { handleGraphQLError } from '@/lib/utils/error-handler'
 
 export async function POST(request: Request) {
     const body = await request.json()
-
     if (body === undefined || body === null) return NextResponse.json({ success: false })
-
     const payload = body.payload || body
     const { table, event } = payload
     const { data, op, session_variables } = event || {}
-
     if (!table || !event) return NextResponse.json({ success: false })
-
+    const nhost = createAdminNhostClient()
     const tableName = table.name
     const { old, new: newData } = data || {}
     const rowId = newData?.id || old?.id
     const meta = newData?.meta || old?.meta || {}
     const userId = session_variables?.['x-hasura-user-id']
-
     if (!userId) return NextResponse.json({ success: false })
-
     const difference = computeDataDifference(old, newData)
-
     const auditLogEntry = createAuditLogEntry({
         operation: op,
         tableName,
@@ -34,13 +35,18 @@ export async function POST(request: Request) {
         meta,
     })
 
-    await insertAuditLog({
-        object: auditLogEntry,
-        on_conflict: {
-            constraint: 'audit_logs_pkey',
-            update_columns: [],
-        },
-    }).catch(handleGraphQLError)
+    await nhost.graphql
+        .request<InsertActivityLogMutation, InsertActivityLogMutationVariables>(
+            gql(InsertActivityLogDocument),
+            {
+                object: auditLogEntry,
+                on_conflict: {
+                    constraint: Activity_Logs_Constraint.ActivityLogsPkey,
+                    update_columns: [],
+                },
+            },
+        )
+        .catch(handleGraphQLError)
 
     return NextResponse.json({ success: true })
 }
